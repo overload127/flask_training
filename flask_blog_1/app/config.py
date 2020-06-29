3d6b3f025f92edc7692e733cd3df1411b30e7e81
Kostya Vinogradov <kostya.06.05.1995@gmail.com>
2020-06-29 20:46:56 +0300

    Завершены 12 уроков по flask

--- flask_blog_1/app/config.py
+++ flask_blog_1/app/config.py
@@ -1,2 +1,16 @@
+import os
+
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
 class Configuration(object):
     DEBUG = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SECRET_KEY = 'something very secret'
+
+    # flask-security
+    SECURITY_PASSWORD_SALT = 'salt'
+    SECURITY_PASSWORD_HASH = 'sha512_crypt'
--- flask_blog_1/app/main.py
+++ flask_blog_1/app/main.py
@@ -1,5 +1,12 @@
 from app import app
+from app import db
+
 import views
+
+from posts.blueprint import posts
+
+
+app.register_blueprint(posts, url_prefix='/blog')
 
 
 if __name__ == '__main__':
